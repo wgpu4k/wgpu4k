e301ebdcd5f197cf81c5a1e8243efa44141da22f
ygdrasil-io <alexandre.mommers@gmail.com>
2024-03-25 00:08:43 +0100

    Add mappings in TextureViewDescriptor
    Implemented mappings from TextureViewDescriptor properties: 'aspect' and 'format' to the respective properties in WGPUTextureViewDescriptor. The EnumerationTransformer has been utilized for these mapping transformations.

--- wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/mapper/TextureViewDescriptor.kt
+++ wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/mapper/TextureViewDescriptor.kt
@@ -7,4 +7,6 @@ import io.ygdrasil.wgpu.mapper
 
 internal val textureViewDescriptorMapper = mapper<TextureViewDescriptor, WGPUTextureViewDescriptor> {
     TextureViewDescriptor::dimension mappedTo WGPUTextureViewDescriptor::dimension withTransformer EnumerationTransformer()
+    TextureViewDescriptor::aspect mappedTo WGPUTextureViewDescriptor::aspect withTransformer EnumerationTransformer()
+    TextureViewDescriptor::format mappedTo WGPUTextureViewDescriptor::format withTransformer EnumerationTransformer()
 }
