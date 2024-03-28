aeb3fda1c2dd9ca62e9c6f7d34f28eb3238abe7a
ygdrasil-io <alexandre.mommers@gmail.com>
2024-03-28 21:55:07 +0100

    Refactor Structure logging and update WGPUVertexState structure
    This commit refactors the logging of com.sun.jna Structure objects with enhanced format and array support. The WGPUVertexState class in Structures.kt is also modified with changes to the 'constantCount' variable type and adjustment of the FieldOrder attribute. The updates are tested in the newly added 'CCodeGeneratorKtTest'.

--- wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/internal/jvm/CCodeGenerator.kt
+++ wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/internal/jvm/CCodeGenerator.kt
@@ -4,23 +4,18 @@ import com.sun.jna.Structure
 import com.sun.jna.Structure.FieldOrder
 import kotlin.reflect.KClass
 import kotlin.reflect.full.memberProperties
-
-class CCodeGenerator {
-
-
-}
+import kotlin.reflect.full.superclasses
 
 
 fun Structure.log(indentation: UInt = 0u, isPointer: Boolean = true): String {
 
-    val fieldOrder = this::class.annotations.find { it is FieldOrder } as? FieldOrder
-        ?: error("fail to find FieldOrder")
+    val fieldOrder = (this::class as KClass<Any>).getFieldOrder()
     val memberProperties = (this::class as KClass<Any>).memberProperties
 
     val log = StringBuilder()
     log.addIndentation(indentation)
     log.append(if (isPointer) "&" else "")
-    log.append("(const ${this::class.simpleName}){\n")
+    log.append("(const ${(this::class as KClass<Any>).getName()}){\n")
 
     fieldOrder.value.forEach { name ->
         val property = memberProperties.find { it.name == name } ?: error("fail to find property")
@@ -33,7 +28,20 @@ fun Structure.log(indentation: UInt = 0u, isPointer: Boolean = true): String {
         } else {
             if (value is Structure) {
                 log.append("\n")
-                log.append(value.log(indentation + 2u), value !is Structure.ByReference)
+                log.append(value.log(indentation + 2u, value is Structure.ByReference))
+            } else if (value is Array<*> && value.isNotEmpty()) {
+                val arrayName = (value.first()!!::class as KClass<Any>).getName()
+                log.append("\n")
+                log.addIndentation(indentation + 2u)
+                log.append("(const $arrayName[]){\n")
+                value
+                    .filterIsInstance<Structure>()
+                    .forEach { log.append(it.log(indentation + 3u, value is Structure.ByReference)) }
+
+
+                log.append("\n")
+                log.addIndentation(indentation + 2u)
+                log.append("}")
             } else if (value is String) {
                 log.append("\"${value}\"")
             } else {
@@ -44,9 +52,23 @@ fun Structure.log(indentation: UInt = 0u, isPointer: Boolean = true): String {
     }
 
     log.addIndentation(indentation)
-    log.append("}\n")
+    log.append("}")
     return log.toString()
 }
+
+private fun KClass<*>.getName(): String? {
+    return if (simpleName == "ByReference") {
+        superclasses.mapNotNull { it.simpleName }.firstOrNull() { it.startsWith("WGPU") }
+    } else {
+        simpleName
+    }
+}
+
+private fun KClass<Any>.getFieldOrder(): FieldOrder = (findFieldOrder()
+    ?: superclasses.mapNotNull { it.findFieldOrder() }.firstOrNull()
+    ?: error("fail to find FieldOrder"))
+
+private fun KClass<*>.findFieldOrder(): FieldOrder? = (annotations.find { it is FieldOrder } as? FieldOrder)
 
 private fun java.lang.StringBuilder.addIndentation(indentation: UInt) {
     repeat(indentation.toInt()) { append("    ") }
--- wgpu4k/src/jvmTest/kotlin/io/ygdrasil/wgpu/internal/jvm/CCodeGeneratorKtTest.kt
+++ wgpu4k/src/jvmTest/kotlin/io/ygdrasil/wgpu/internal/jvm/CCodeGeneratorKtTest.kt
@@ -3,6 +3,9 @@ package io.ygdrasil.wgpu.internal.jvm
 import com.sun.jna.Pointer
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import io.ygdrasil.wgpu.ColorWriteMask
+import io.ygdrasil.wgpu.PrimitiveTopology
+import io.ygdrasil.wgpu.TextureFormat
 
 class CCodeGeneratorKtTest : FreeSpec({
 
@@ -10,46 +13,79 @@ class CCodeGeneratorKtTest : FreeSpec({
         WGPURenderPipelineDescriptor().apply {
             label = "render_pipeline"
             layout = WGPUPipelineLayout(Pointer(1))
+
+            vertex = WGPUVertexState()
             vertex?.entryPoint = "vs_main"
+            vertex?.module = WGPUShaderModuleImpl(Pointer(1))
+
+            primitive = WGPUPrimitiveState()
+            primitive?.topology = PrimitiveTopology.trianglelist.value
+
+            multisample = WGPUMultisampleState()
+            multisample?.count = 1
+            multisample?.mask = 0xFFFFFFF
+
+            fragment = WGPUFragmentState.ByReference()
+            fragment?.module = WGPUShaderModuleImpl(Pointer(1))
+            fragment?.entryPoint = "fs_main"
+            fragment?.targets = arrayOf(
+                WGPUColorTargetState.ByReference().apply {
+                    format = TextureFormat.depth24plus.value
+                    writeMask = ColorWriteMask.all.value
+                }
+            )
+
+            write()
         }.log() shouldBe """
              |&(const WGPURenderPipelineDescriptor){
              |    .nextInChain = NULL,
              |    .label = "render_pipeline",
-             |    .layout = pipeline_layout,
+             |    .layout = native@0x1 (io.ygdrasil.wgpu.internal.jvm.WGPUPipelineLayoutImpl@1),
              |    .vertex = 
              |        (const WGPUVertexState){
              |            .nextInChain = NULL,
-             |            .module = shader_module,
+             |            .module = native@0x1 (io.ygdrasil.wgpu.internal.jvm.WGPUShaderModuleImpl@1),
              |            .entryPoint = "vs_main",
-             |            .constantCount = 0,
-             |            .constants = NULL,
-             |            .bufferCount = 0,
+             |            .constantCount = NULL,
+             |            .constantsPtr = NULL,
+             |            .bufferCount = NULL,
              |            .buffersPtr = NULL,
              |        },
              |    .primitive = 
              |        (const WGPUPrimitiveState){
-             |            .topology = WGPUPrimitiveTopology_TriangleList,
+             |            .nextInChain = NULL,
+             |            .topology = 3,
+             |            .stripIndexFormat = NULL,
+             |            .frontFace = NULL,
+             |            .cullMode = NULL,
              |        },
              |    .depthStencil = NULL,
              |    .multisample = 
              |        (const WGPUMultisampleState){
+             |            .nextInChain = NULL,
              |            .count = 1,
-             |            .mask = 0xFFFFFFFF,
+             |            .mask = 268435455,
+             |            .alphaToCoverageEnabled = NULL,
              |        },
              |    .fragment = 
              |        &(const WGPUFragmentState){
-             |            .module = shader_module,
+             |            .nextInChain = NULL,
+             |            .module = native@0x1 (io.ygdrasil.wgpu.internal.jvm.WGPUShaderModuleImpl@1),
              |            .entryPoint = "fs_main",
+             |            .constantCount = 0,
+             |            .constants = NULL,
              |            .targetCount = 1,
-             |            .targets =
+             |            .targets = 
              |                (const WGPUColorTargetState[]){
              |                    (const WGPUColorTargetState){
-             |                        .format = surface_capabilities.formats[0],
-             |                        .writeMask = WGPUColorWriteMask_All,
-             |                    },
+             |                        .nextInChain = NULL,
+             |                        .format = 40,
+             |                        .blend = NULL,
+             |                        .writeMask = 15,
+             |                    }
              |                },
              |        },
-             |};
+             |}
       """.trimMargin()
 
     }
