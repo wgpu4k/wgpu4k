38fbf594bf3014ccdab8159a683cb147c577c20e
ygdrasil-io <alexandre.mommers@gmail.com>
2024-05-05 13:17:29 +0200

    Remove unused resource files
    Removed two unused resources files, 'readme.txt' inside 'cubemap' directory and a json font file 'ya-hei-ascii-msdf.json'. This is done to cleanup the repository and to avoid confusion about the project's dependencies.

--- examples/web-js/build.gradle.kts
+++ examples/web-js/build.gradle.kts
@@ -1,22 +1,44 @@
 plugins {
-	alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinMultiplatform)
 }
+
+val commonResourcesFile = getCommonProject()
+    .projectDir
+    .resolve("src")
+    .resolve("commonMain")
+    .resolve("resources")
+
+assert(commonResourcesFile.isDirectory) { "$commonResourcesFile is not a directory" }
+assert(commonResourcesFile.isNotEmpty) { "$commonResourcesFile is empty" }
 
 kotlin {
-	js {
-		binaries.executable()
-		browser()
-		nodejs()
-		generateTypeScriptDefinitions()
-	}
+    js {
+        binaries.executable()
+        browser()
+        nodejs()
+        generateTypeScriptDefinitions()
+    }
 
-	sourceSets {
-		val commonMain by getting {
-			dependencies {
-				implementation(projects.examples.common)
-			}
-		}
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(projects.examples.common)
+            }
+        }
 
-	}
+        val jsMain by getting {
+            resources.setSrcDirs(
+                resources.srcDirs + setOf(
+                    commonResourcesFile
+                )
+            )
+        }
+
+    }
 }
 
+fun getCommonProject() = projects.examples.common.identityPath.path
+    ?.let(::project) ?: error("Could not find project path")
+
+val File.isNotEmpty: Boolean
+    get() = this.listFiles()?.isNotEmpty() ?: false
