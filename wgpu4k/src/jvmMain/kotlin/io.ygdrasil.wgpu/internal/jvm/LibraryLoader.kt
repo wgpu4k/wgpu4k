159f7ef858dda7f7767b4ca45bdf1ef9ddda6138
ygdrasil-io <alexandre.mommers@gmail.com>
2024-05-27 22:34:49 +0200

    Enhance library loader for different OS
    The library loader in the WGPU package has been extended to handle loading on Windows and other Operating Systems. Added support for extracting WGPU libraries for Windows and logic for finding a writable path. Also, improved error messaging in case resource files are not found on the classpath.

--- wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/internal/jvm/LibraryLoader.kt
+++ wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/internal/jvm/LibraryLoader.kt
@@ -1,21 +1,44 @@
 package io.ygdrasil.wgpu.internal.jvm
 
 import io.ygdrasil.wgpu.WGPU
+import java.io.File
 import java.io.InputStream
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.StandardCopyOption
 
 internal fun exportAndLoadLibrary() {
+    val libraryPath = findLibraryPath()
     when (Platform.os) {
-        Os.Windows -> System.loadLibrary("WGPU")
+        Os.Windows -> {
+            val libraryFile = inferWindowsWGPUPossiblePath()
+            extractResourceToTemp(libraryPath, libraryFile)
+            System.loadLibrary(libraryFile.nameWithoutExtension)
+        }
         else -> {
-            val libraryPath = findLibraryPath()
-            val libraryFile =
-                extractResourceToTemp(libraryPath)?.toFile() ?: error("fail to find webgpu library")
+            val libraryFile = generateTempFile()
+            extractResourceToTemp(libraryPath, generateTempFile())
             System.load(libraryFile.absolutePath)
         }
     }
+}
+
+private fun inferWindowsWGPUPossiblePath(): File {
+    val possiblePaths = libraryPaths().map { File(it) }
+    listWritablePathOn(possiblePaths).forEach { path ->
+        (0 until 100).forEach { index ->
+            path.resolve("WGPU$index.dll")
+                .takeIf { it.canWrite() && it.exists().not() }
+                ?.let { return it }
+        }
+    }
+
+    error("fail to find writable path, put one java.library.path")
+}
+
+private fun libraryPaths() = System.getProperty("java.library.path").split(File.pathSeparator)
+
+private fun listWritablePathOn(files: List<File>): List<File> {
+    return files.filter { it.canWrite() }
 }
 
 private fun findLibraryPath(): String {
@@ -42,22 +65,24 @@ private fun findLibraryPath(): String {
     return "/$os-$architecture/$libraryName"
 }
 
-private fun extractResourceToTemp(fileOnClasspath: String): Path? {
+private fun generateTempFile(): File {
+    // create a temp file
+    val tempFile = Files.createTempFile("wgpu", "lib").toFile()
+    println(tempFile.absolutePath)
+    return tempFile
+}
+
+private fun extractResourceToTemp(fileOnClasspath: String, target: File) {
+    println("will extract library to path ${target.absolutePath}")
+
     // fetch file from the classpath
     val resourceAsStream: InputStream? = WGPU::class.java.getResourceAsStream(fileOnClasspath)
 
     if (resourceAsStream == null) {
-        println("Could not find file $fileOnClasspath on the classpath")
-        return null
+        error("Could not find file $fileOnClasspath on the classpath")
     }
 
-    // create a temp file
-    val tempFile = Files.createTempFile("wgpu", "lib")
-    println(tempFile.toFile().absolutePath)
-    tempFile.toFile().deleteOnExit()
-
     // copy the file to the temp directory
-    Files.copy(resourceAsStream, tempFile, StandardCopyOption.REPLACE_EXISTING)
-
-    return tempFile
+    Files.copy(resourceAsStream, target.toPath(), StandardCopyOption.REPLACE_EXISTING)
+    target.deleteOnExit()
 }
