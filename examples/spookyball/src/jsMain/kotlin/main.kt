a019530d32b6a333a3ed6f1a6fad72b288808300
ygdrasil-io <alexandre.mommers@gmail.com>
2024-05-30 21:03:21 +0200

    Update canvas selection method and remove unused HTML elements
    The canvas selection method in main.kt has been updated by using 'querySelector' rather than 'getElementById'. The 'left-banner' and 'right-banner' elements in index.html along with related CSS were removed as they're no longer necessary. Furthermore, the script source in index.html has been changed from 'wgpu4k-root-examples-spookyball.js' to 'spookyball.js'.

--- examples/spookyball/src/jsMain/kotlin/main.kt
+++ examples/spookyball/src/jsMain/kotlin/main.kt
@@ -1,13 +1,12 @@
-import io.ygdrasil.wgpu.examples.jsApplication
 import kotlinx.browser.document
 import kotlinx.browser.window
 import org.w3c.dom.HTMLCanvasElement
 
 fun main() {
 	window.addEventListener("DOMContentLoaded", {
-		val canvas = (document.getElementById("webgpu") as? HTMLCanvasElement) ?: error("fail to get canvas")
-		jsApplication(canvas)
-			.then { it.run() }
+		val canvas = (document.querySelector("canvas") as? HTMLCanvasElement) ?: error("fail to get canvas")
+		/*jsApplication(canvas)
+			.then { it.run() }*/
 
 	})
 }
