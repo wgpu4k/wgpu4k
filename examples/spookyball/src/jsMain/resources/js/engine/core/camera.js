b9fa6bbe9df1ac46f018471ee8081126a1dfcd5e
ygdrasil-io <alexandre.mommers@gmail.com>
2024-06-12 22:01:45 +0200

    Remove redundant JavaScript and Kotlin files
    Several JavaScript files and one Kotlin file, all redundantly centered around managing balls and animations in a SpookyBall example, were removed. These files consisted of ball.js, animation.js, .eslintrc.json, basis-worker.js, basis_transcoder.js in the JavaScript directory, and BallSystem.kt in the Kotlin directory. Their functionalities were either obsolete or had been integrated into other parts of the code.

--- examples/spookyball/src/jsMain/resources/js/engine/core/camera.js
+++
@@ -1,5 +0,0 @@
-export class Camera {
-  fieldOfView = Math.PI * 0.5;
-  zNear = 0.01;
-  zFar = 1000.0;
-}
--- examples/spookyball/src/jsMain/resources/js/engine/webgpu/webgpu-system.js
+++
@@ -1,6 +0,0 @@
-import {System} from '../core/ecs.js';
-import {Stage} from '../core/stage.js';
-
-export class WebGPUSystem extends System {
-  stage = Stage.Render;
-};
