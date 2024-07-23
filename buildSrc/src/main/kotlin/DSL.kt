78a57defdbdd5ec33155f0e0264bff4b1b3e5998
Alexandre Mommers <alexandre.mommers@gmail.com>
2024-07-23 16:34:23 +0200

    Add fetch-native-dependencies task in build process
    A new Gradle task, "fetch-native-dependencies", has been added in DSL.kt to manage native dependencies fetching. This task has been integrated into the main build process in test.yml, ensuring that the dependencies are fetched before the build and check processes are run.

--- buildSrc/src/main/kotlin/DSL.kt
+++ buildSrc/src/main/kotlin/DSL.kt
@@ -15,19 +15,23 @@ class DSL internal constructor(val project: Project) {
 
     var baseUrl: String? = null
     var zipBuildDirectory = project.file("build").resolve("zip")
+    val fetchTask = project.tasks.register("fetch-native-dependencies") {
+        group = "wgpu4k"
+    }
 
     fun download(fileName: String, action: DownloadContext.() -> Unit) {
         val baseUrl = baseUrl ?: error("baseUrl must be set")
 
         val zipFile = zipBuildDirectory.resolve(fileName)
         val downloadTask = project.downloadInto(baseUrl, fileName, zipFile)
-        action(DownloadContext(project, downloadTask, zipFile))
+        action(DownloadContext(project, downloadTask, zipFile, fetchTask.get()))
     }
 
     class DownloadContext internal constructor(
         private val project: Project,
         private val downloadTask: Task,
-        private val zipFile: File
+        private val zipFile: File,
+        private val fetchTask: Task
     ) {
 
         fun extract(fileName: String, target: File): Copy {
@@ -43,6 +47,8 @@ class DSL internal constructor(val project: Project) {
                     dependsOn(unzipTask)
                 }
             }
+
+            fetchTask.dependsOn(unzipTask)
 
             return unzipTask
         }
