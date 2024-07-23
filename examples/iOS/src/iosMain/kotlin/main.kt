e9470c71f0ba491221ad9cf006661fdddca9aa06
Alexandre Mommers <alexandre.mommers@gmail.com>
2024-07-23 18:35:41 +0200

    Refactor iOS examples: consolidate main.kt files
    Deleted main.kt files for different iOS ARM architectures as it was redundant. These functions have been combined into a single main.kt within the iosMain directory. The consolidation simplifies the architecture, improves the maintainability of the code, and doesn't affect its execution.

--- examples/iOS/src/iosMain/kotlin/main.kt
+++ examples/iOS/src/iosMain/kotlin/main.kt
@@ -1,8 +1,20 @@
 @file:OptIn(ExperimentalForeignApi::class)
 
+import io.ygdrasil.wgpu.Surface
+import io.ygdrasil.wgpu.WGPU
 import kotlinx.cinterop.COpaquePointer
 import kotlinx.cinterop.ExperimentalForeignApi
 
-expect fun nothing()
+fun initwgpu(metalLayer: COpaquePointer) {
+    val sizeProvider = {
+        100 to 100
+    }
+    val instance = WGPU.createInstance() ?: error("Can't create WGPU instance")
+    val surface = instance.getSurfaceFromMetalLayer(metalLayer)
+        ?.let { Surface(it, sizeProvider) } ?: error("Can't create Surface")
+    val adapter = instance.requestAdapter(surface) ?: error("Can't create Adapter")
+}
 
-expect fun initwgpu(metalLayer: COpaquePointer)
+fun nothing() {
+    println("nothing")
+}
