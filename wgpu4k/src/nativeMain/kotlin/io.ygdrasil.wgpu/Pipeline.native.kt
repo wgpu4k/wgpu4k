25857291cde6626fef8369f0fdc6a863189b44b6
Alexandre Mommers <alexandre.mommers@gmail.com>
2024-07-26 18:45:01 +0200

    Implement Native Pipeline Methods and Fix Queue Functions
    Implemented core functions for native RenderPipeline, including `getBindGroupLayout` and `close`. Corrected and completed methods in the Queue class for buffer writes and image copying, ensuring functionality aligns with the required specifications.

--- wgpu4k/src/nativeMain/kotlin/io.ygdrasil.wgpu/Pipeline.native.kt
+++ wgpu4k/src/nativeMain/kotlin/io.ygdrasil.wgpu/Pipeline.native.kt
@@ -5,14 +5,18 @@ package io.ygdrasil.wgpu
 import kotlinx.cinterop.ExperimentalForeignApi
 import webgpu.WGPUPipelineLayout
 import webgpu.WGPURenderPipeline
+import webgpu.wgpuRenderPipelineGetBindGroupLayout
+import webgpu.wgpuRenderPipelineRelease
 
 actual class RenderPipeline(internal val handler: WGPURenderPipeline) : AutoCloseable {
+
     actual fun getBindGroupLayout(index: Int): BindGroupLayout {
-        TODO("Not yet implemented")
+        return wgpuRenderPipelineGetBindGroupLayout(handler, index.toUInt())
+            ?.let { BindGroupLayout(it) } ?: error("fail to get bindgroup layout")
     }
 
     actual override fun close() {
-        TODO("Not yet implemented")
+        wgpuRenderPipelineRelease(handler)
     }
 
 }
--- wgpu4k/src/nativeMain/kotlin/io.ygdrasil.wgpu/Queue.native.kt
+++ wgpu4k/src/nativeMain/kotlin/io.ygdrasil.wgpu/Queue.native.kt
@@ -2,13 +2,30 @@
 
 package io.ygdrasil.wgpu
 
-import kotlinx.cinterop.ExperimentalForeignApi
-import webgpu.WGPUQueue
+import io.ygdrasil.wgpu.internal.toPointerArray
+import io.ygdrasil.wgpu.mapper.map
+import kotlinx.cinterop.*
+import webgpu.*
+
+private val supportedFormatOncopyExternalImageToTexture = listOf(TextureFormat.rgba8unorm, TextureFormat.rgba8unormsrgb)
 
 actual class Queue(internal val handler: WGPUQueue) {
 
-    actual fun submit(commandsBuffer: List<CommandBuffer>) {
-        TODO("Not yet implemented")
+    actual fun submit(commandsBuffer: List<CommandBuffer>) = memScoped {
+        if (commandsBuffer.isNotEmpty()) {
+            val commands = commandsBuffer.map { it.handler }.toPointerArray(this)
+            wgpuQueueSubmit(
+                handler,
+                commandsBuffer.size.toULong(),
+                commands
+            )
+        } else {
+            wgpuQueueSubmit(
+                handler,
+                0uL,
+                null
+            )
+        }
     }
 
     actual fun writeBuffer(
@@ -17,8 +34,14 @@ actual class Queue(internal val handler: WGPUQueue) {
         data: FloatArray,
         dataOffset: GPUSize64,
         size: GPUSize64
-    ) {
-        TODO("Not yet implemented")
+    ) = memScoped {
+        wgpuQueueWriteBuffer(
+            handler,
+            buffer.handler,
+            bufferOffset.toULong(),
+            data.toBuffer(dataOffset, this),
+            (size * Float.SIZE_BYTES).toULong()
+        )
     }
 
     actual fun writeBuffer(
@@ -27,24 +50,76 @@ actual class Queue(internal val handler: WGPUQueue) {
         data: IntArray,
         dataOffset: GPUSize64,
         size: GPUSize64
-    ) {
-        TODO("Not yet implemented")
+    ) = memScoped {
+        wgpuQueueWriteBuffer(
+            handler,
+            buffer.handler,
+            bufferOffset.toULong(),
+            data.toBuffer(dataOffset, this),
+            (size * Float.SIZE_BYTES).toULong()
+        )
     }
 
     actual fun copyExternalImageToTexture(
         source: ImageCopyExternalImage,
         destination: ImageCopyTextureTagged,
         copySize: GPUIntegerCoordinates
-    ) {
-        TODO("Not yet implemented")
+    ) = memScoped {
+        check(destination.texture.format in supportedFormatOncopyExternalImageToTexture) {
+            "(${
+                supportedFormatOncopyExternalImageToTexture.map { it.actualName }.joinToString(", ")
+            })are the only supported texture format supported, found ${destination.texture.format}"
+        }
+
+        val image = (source.source as? ImageBitmapHolder)
+            ?: error("ImageBitmapHolder required as source")
+
+        val bytePerPixel = destination.texture.format.getBytesPerPixel()
+
+        wgpuQueueWriteTexture(
+            handler,
+            map(destination).ptr,
+            image.data,
+            (image.width * bytePerPixel * image.height).toULong(),
+            alloc<WGPUTextureDataLayout>().also { dataLayout ->
+                dataLayout.offset = 0u
+                dataLayout.bytesPerRow = (image.width * bytePerPixel).toUInt()
+                dataLayout.rowsPerImage = image.height.toUInt()
+            }.ptr,
+            alloc<WGPUExtent3D>().also { size3D ->
+                size3D.width = image.width.toUInt()
+                size3D.height = image.height.toUInt()
+                size3D.depthOrArrayLayers = 1u
+            }.ptr
+        )
+
     }
 
+    private fun FloatArray.toBuffer(dataOffset: GPUSize64, arena: ArenaBase): CValuesRef<*> {
+        if (dataOffset != 0L) error("data offset not yet supported") // TODO support dataOffset
+        return arena.allocArray<FloatVar>(size).also {
+            forEachIndexed { index, value -> it[index] = value }
+        }
+    }
+
+    private fun IntArray.toBuffer(dataOffset: GPUSize64, arena: ArenaBase): CValuesRef<*> {
+        if (dataOffset != 0L) error("data offset not yet supported") // TODO support dataOffset
+        return arena.allocArray<IntVar>(size).also {
+            forEachIndexed { index, value -> it[index] = value }
+        }
+    }
 }
 
 actual sealed interface DrawableHolder
-actual class ImageBitmapHolder : DrawableHolder {
-    actual val width: Int
-        get() = TODO("Not yet implemented")
+
+actual class ImageBitmapHolder(
+    val arena: Arena,
+    val data: CValuesRef<*>,
+    actual val width: Int,
     actual val height: Int
-        get() = TODO("Not yet implemented")
+) : DrawableHolder, AutoCloseable {
+
+    override fun close() {
+        arena.clear()
+    }
 }
