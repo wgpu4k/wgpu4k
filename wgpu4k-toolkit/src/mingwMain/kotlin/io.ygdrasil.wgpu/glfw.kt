0361f354fbe32b3d42e7305ca8e8ffeccb495b06
Alexandre Mommers <alexandre.mommers@gmail.com>
2024-08-03 14:50:00 +0200

    Refactor `getSurface` error handling in glfw.kt
    Simplify error handling logic by integrating null checks and improving error messages for `hwnd` and `hinstance`. The new implementation uses Kotlin idioms for better readability and maintainability.

--- wgpu4k-toolkit/src/mingwMain/kotlin/io.ygdrasil.wgpu/glfw.kt
+++ wgpu4k-toolkit/src/mingwMain/kotlin/io.ygdrasil.wgpu/glfw.kt
@@ -2,12 +2,14 @@ package io.ygdrasil.wgpu
 
 import cnames.structs.GLFWwindow
 import glfw.glfwGetWin32Window
-import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.*
 import platform.windows.GetModuleHandle
 
 actual fun WGPU.getSurface(window: CPointer<GLFWwindow>, sizeProvider: () -> Pair<Int, Int>): Surface {
-    val hwnd = glfwGetWin32Window(window)
-    val hinstance = GetModuleHandle?(null)?.reinterpret<COpaquePointer>() ?: error("fail to get hinstance")
+    val hwnd = glfwGetWin32Window(window)  ?: error("fail to get hwnd")
+    val hinstance: COpaquePointer = GetModuleHandle?.invoke(null)
+        ?.let { interpretCPointer<COpaque>(it.rawValue) }
+        ?.reinterpret() ?: error("fail to get hinstance")
     val surface = getSurfaceFromWindows(hinstance, hwnd) ?: error("fail to get surface on Windows")
     return Surface(surface, sizeProvider)
 }
