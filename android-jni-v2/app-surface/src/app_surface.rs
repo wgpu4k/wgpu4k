3c72370c8a4074e55a96747da5774281d20c12ad
ygdrasil-io <alexandre.mommers@gmail.com>
2024-08-07 23:23:08 +0200

    Refactor Android JNI integration
    Refactored the Android JNI setup to improve modularity and maintainability. Split existing code into multiple crates and updated configurations. Added README instructions for setting up and building on Android.

---
+++ android-jni-v2/app-surface/src/app_surface.rs
@@ -0,0 +1,130 @@
+use core::ffi::c_void;
+use std::sync::{Arc, Mutex};
+
+use jni::JNIEnv;
+use jni::sys::jobject;
+use raw_window_handle::{
+    AndroidDisplayHandle, AndroidNdkWindowHandle, DisplayHandle, HandleError, HasDisplayHandle,
+    HasWindowHandle, RawDisplayHandle, RawWindowHandle, WindowHandle,
+};
+
+pub struct AppSurface {
+    pub native_window: Arc<NativeWindow>,
+    pub scale_factor: f32,
+    pub sdq: crate::SurfaceDeviceQueue,
+    pub instance: *const wgpu::Instance,
+    pub callback_to_app: Option<extern "C" fn(arg: i32)>,
+}
+
+impl AppSurface {
+    pub fn new(wgpu: *const wgpu::Instance, env: *mut JNIEnv, surface: jobject) -> Self {
+        let native_window = Arc::new(NativeWindow::new(env, surface));
+        let handle: Box<dyn wgpu::WindowHandle> = Box::new(native_window.clone());
+        let wgpu_ref: &wgpu::Instance = unsafe { &*wgpu };
+        let surface = wgpu_ref
+            .create_surface(wgpu::SurfaceTarget::Window(handle))
+            .unwrap();
+        let (adapter, device, queue) =
+            pollster::block_on(crate::request_device(wgpu_ref, &surface));
+
+        let caps = surface.get_capabilities(&adapter);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            width: native_window.get_width(),
+            height: native_window.get_height(),
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            native_window,
+            scale_factor: 1.0,
+            sdq: crate::SurfaceDeviceQueue {
+                surface: Arc::new(surface),
+                config,
+                adapter: Arc::new(adapter),
+                device: Arc::new(device),
+                queue: Arc::new(queue),
+            },
+            instance: wgpu,
+            callback_to_app: None,
+        }
+    }
+
+    pub fn get_view_size(&self) -> (u32, u32) {
+        (
+            self.native_window.get_width(),
+            self.native_window.get_height(),
+        )
+    }
+}
+
+pub struct NativeWindow {
+    a_native_window: Arc<Mutex<*mut ndk_sys::ANativeWindow>>,
+}
+
+impl NativeWindow {
+    fn new(env: *mut JNIEnv, surface: jobject) -> Self {
+        let a_native_window = unsafe {
+            // 获取与安卓端 surface 对象关联的 ANativeWindow，以便能通过 Rust 与之交互。
+            // 此函数在返回 ANativeWindow 的同时会自动将其引用计数 +1，以防止该对象在安卓端被意外释放。
+            ndk_sys::ANativeWindow_fromSurface(env as *mut _, surface as *mut _)
+        };
+        Self {
+            a_native_window: Arc::new(Mutex::new(a_native_window)),
+        }
+    }
+
+    pub fn get_raw_window(&self) -> *mut ndk_sys::ANativeWindow {
+        let a_native_window = self.a_native_window.lock().unwrap();
+        *a_native_window
+    }
+
+    fn get_width(&self) -> u32 {
+        unsafe { ndk_sys::ANativeWindow_getWidth(*self.a_native_window.lock().unwrap()) as u32 }
+    }
+
+    fn get_height(&self) -> u32 {
+        unsafe { ndk_sys::ANativeWindow_getHeight(*self.a_native_window.lock().unwrap()) as u32 }
+    }
+}
+
+impl Drop for NativeWindow {
+    fn drop(&mut self) {
+        unsafe {
+            ndk_sys::ANativeWindow_release(*self.a_native_window.lock().unwrap());
+        }
+    }
+}
+
+impl HasWindowHandle for NativeWindow {
+    fn window_handle(&self) -> Result<WindowHandle, HandleError> {
+        unsafe {
+            let a_native_window = self.a_native_window.lock().unwrap();
+            let handle = AndroidNdkWindowHandle::new(
+                std::ptr::NonNull::new(*a_native_window as *mut _ as *mut c_void).unwrap(),
+            );
+            Ok(WindowHandle::borrow_raw(RawWindowHandle::AndroidNdk(
+                handle,
+            )))
+        }
+    }
+}
+
+impl HasDisplayHandle for NativeWindow {
+    fn display_handle(&self) -> Result<DisplayHandle<'_>, HandleError> {
+        unsafe {
+            Ok(DisplayHandle::borrow_raw(RawDisplayHandle::Android(
+                AndroidDisplayHandle::new(),
+            )))
+        }
+    }
+}
+
+unsafe impl Send for NativeWindow {}
+unsafe impl Sync for NativeWindow {}
--- android-jni-v2/wgpu-in-app/src/ffi/mod.rs
+++ android-jni-v2/wgpu-in-app/src/ffi/mod.rs
@@ -1,16 +1,28 @@
 use android_logger::Config;
+use app_surface::AppSurface;
 use jni::JNIEnv;
 use jni::objects::JClass;
 use jni::sys::{jint, jlong, jobject};
 use jni_fn::jni_fn;
 use log::{info, LevelFilter};
-
-use app_surface::AppSurface;
+use wgpu_native::wgpuCreateInstance;
 
 use crate::wgpu_canvas::WgpuCanvas;
 
 #[no_mangle]
-#[jni_fn("name.jinleili.wgpu.RustBridge")]
+#[jni_fn("io.ygdrasil.wgpu.RustBridge")]
+pub fn createWgpuInstance2(_: *mut JNIEnv, _: JClass) -> jlong {
+    log_panics::init();
+    android_logger::init_once(Config::default().with_max_level(LevelFilter::Error));
+
+    let instance = unsafe { wgpuCreateInstance(None); };
+
+    info!("WgpuCanvas instance!");
+    Box::into_raw(Box::new(instance)) as jlong
+}
+
+#[no_mangle]
+#[jni_fn("io.ygdrasil.wgpu.RustBridge")]
 pub fn createWgpuInstance(_: *mut JNIEnv, _: JClass) -> jlong {
     log_panics::init();
     android_logger::init_once(Config::default().with_max_level(LevelFilter::Error));
@@ -25,24 +37,25 @@ pub fn createWgpuInstance(_: *mut JNIEnv, _: JClass) -> jlong {
 }
 
 #[no_mangle]
-#[jni_fn("name.jinleili.wgpu.RustBridge")]
-pub fn createWgpuCanvas(env: *mut JNIEnv, _: JClass, surface: jobject, idx: jint) -> jlong {
+#[jni_fn("io.ygdrasil.wgpu.RustBridge")]
+pub fn createWgpuCanvas(env: *mut JNIEnv, _: JClass, wgpu: jlong, surface: jobject, idx: jint) -> jlong {
+    let wgpu = unsafe { wgpu as *const wgpu::Instance };
     log_panics::init();
     android_logger::init_once(Config::default().with_max_level(LevelFilter::Error));
-    let canvas = WgpuCanvas::new(AppSurface::new(env as *mut _, surface), idx);
+    let canvas = WgpuCanvas::new(AppSurface::new(wgpu, env as *mut _, surface), idx);
     info!("WgpuCanvas created!");
     Box::into_raw(Box::new(canvas)) as jlong
 }
 
 #[no_mangle]
-#[jni_fn("name.jinleili.wgpu.RustBridge")]
+#[jni_fn("io.ygdrasil.wgpu.RustBridge")]
 pub fn enterFrame(_env: *mut JNIEnv, _: JClass, obj: jlong) {
     let obj = unsafe { &mut *(obj as *mut WgpuCanvas) };
     obj.enter_frame();
 }
 
 #[no_mangle]
-#[jni_fn("name.jinleili.wgpu.RustBridge")]
+#[jni_fn("io.ygdrasil.wgpu.RustBridge")]
 pub fn changeExample(_env: *mut JNIEnv, _: JClass, obj: jlong, idx: jint) {
     // 获取到指针指代的 Rust 对象的可变借用
     let obj = unsafe { &mut *(obj as *mut WgpuCanvas) };
@@ -50,7 +63,7 @@ pub fn changeExample(_env: *mut JNIEnv, _: JClass, obj: jlong, idx: jint) {
 }
 
 #[no_mangle]
-#[jni_fn("name.jinleili.wgpu.RustBridge")]
+#[jni_fn("io.ygdrasil.wgpu.RustBridge")]
 pub fn dropWgpuCanvas(_env: *mut JNIEnv, _: JClass, obj: jlong) {
     let _obj: Box<WgpuCanvas> = unsafe { Box::from_raw(obj as *mut _) };
 }
