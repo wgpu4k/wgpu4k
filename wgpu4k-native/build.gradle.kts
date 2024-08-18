ca757d118554381b20f489377a3cf9e9774ab7f0
ygdrasil-io <alexandre.mommers@gmail.com>
2024-08-18 02:08:36 +0200

    Add Android and iOS support for WebGPU bindings
    Introduced new Kotlin Native bindings for Android and updated build scripts to handle static libraries for Android and iOS platforms. Enhanced download tasks to include necessary WebGPU binaries for both Android and iOS architectures.

--- wgpu4k-native/build.gradle.kts
+++ wgpu4k-native/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 }
 
 val buildNativeResourcesDirectory = project.file("build").resolve("native")
-val resourcesDirectory = project.file("src").resolve("jvmMain").resolve("resources")
 
 kotlin {
 
@@ -20,6 +19,8 @@ kotlin {
         tvosX64(),
         linuxArm64(),
         linuxX64(),
+        androidNativeX64(),
+        androidNativeArm64(),
         configureMingwX64(),
     ).filterNotNull()
 
@@ -40,25 +41,45 @@ kotlin {
 configureDownloadTasks {
     baseUrl = "${project.properties["wgpu.base.url"]}${libs.versions.wgpu.get()}/"
 
+    /*** Macos ***/
     download("wgpu-macos-aarch64-release.zip") {
         extract("webgpu.h", buildNativeResourcesDirectory.resolve("webgpu.h"))
         extract("wgpu.h", buildNativeResourcesDirectory.resolve("wgpu.h"))
         extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("darwin-aarch64").resolve("libWGPU.a"))
     }
-
     download("wgpu-macos-x86_64-release.zip") {
         extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("darwin-x64").resolve("libWGPU.a"))
     }
 
+    /*** Windows ***/
     download("wgpu-windows-x86_64-gnu-release.zip") {
         extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("windows-x64").resolve("wgpu.a"))
     }
 
+    /*** Linux ***/
     download("wgpu-linux-x86_64-release.zip") {
         extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("linux-x64").resolve("libWGPU.a"))
     }
-
     download("wgpu-linux-aarch64-release.zip") {
         extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("linux-aarch64").resolve("libWGPU.a"))
+    }
+
+    /*** Android ***/
+    download("wgpu-android-x86_64-release.zip") {
+        extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("android-x64").resolve("libWGPU.a"))
+    }
+    download("wgpu-android-aarch64-release.zip") {
+        extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("android-aarch64").resolve("libWGPU.a"))
+    }
+
+    /*** iOS ***/
+    download("wgpu-iOS-x86_64-simulator-release.zip") {
+        extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("ios-simulator-x64").resolve("libWGPU.a"))
+    }
+    download("wgpu-iOS-aarch64-simulator-release.zip") {
+        extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("ios-simulator-aarch64").resolve("libWGPU.a"))
+    }
+    download("wgpu-iOS-aarch64-release.zip") {
+        extract("libwgpu_native.a", buildNativeResourcesDirectory.resolve("ios-aarch64").resolve("libWGPU.a"))
     }
 }
