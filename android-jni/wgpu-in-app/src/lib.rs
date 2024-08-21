61b68edc25acd7c1d1759b76fbb23937233b6392
ygdrasil-io <alexandre.mommers@gmail.com>
2024-08-21 13:18:04 +0200

    Remove unused files: libgcc.a and 8x8.astc
    Deleted an unused static library (`libgcc.a`) and an asset (`8x8.astc`) from the project. This cleanup helps to reduce clutter and maintain a leaner codebase.

--- android-jni/wgpu-in-app/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub use ffi::*;
-pub use wgpu_canvas::WgpuCanvas;
-
-mod examples;
-mod wgpu_canvas;
-mod ffi;
-
--- android-jni/wgpu-in-app/src/main.rs
+++
@@ -1,9 +0,0 @@
-#![allow(clippy::single_match)]
-
-#[cfg(any(target_os = "android", target_os = "ios"))]
-fn main() {}
-
-#[cfg(any(target_os = "macos", target_os = "windows", target_os = "linux"))]
-fn main() -> Result<(), impl std::error::Error> {
-    wgpu_in_app::desktop::run()
-}
