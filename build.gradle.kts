592ea5c221c7e7b490b6480d8376c9ddf940790d
ygdrasil-io <alexandre.mommers@gmail.com>
2024-08-22 00:39:14 +0200

    Remove unnecessary Android plugin configuration checks
    Simplified the build script by eliminating redundant checks for whether the Android plugin is configured. This change streamlines the build process and reduces script complexity.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,7 @@
-import com.android.utils.TraceUtils.simpleId
 
 plugins {
 	alias(libs.plugins.compose.compiler) apply false
 	alias(libs.plugins.compose) apply false
-}
-
-tasks.findByName("checkKotlinGradlePluginConfigurationErrors").also {
-	if (isAndroidConfigured) {
-		println("android is configured will keep checkKotlinGradlePluginConfigurationErrors $it")
-	} else {
-		println("android is not configured will disable checkKotlinGradlePluginConfigurationErrors $it")
-	}
-	it?.enabled = isAndroidConfigured
 }
 
 allprojects {
