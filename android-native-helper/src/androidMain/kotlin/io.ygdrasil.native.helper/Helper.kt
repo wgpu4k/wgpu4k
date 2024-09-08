34892596e046faf262f8746961dccad813bb58fc
ygdrasil-io <alexandre.mommers@gmail.com>
2024-09-08 19:07:35 +0200

    Remove deprecated Android native helper and JNI code
    This commit deletes the deprecated Android native helper and JNI-related files from the project. These include the `Adapter.kt`, `build.gradle.kts`, and helper files associated with `android-native-helper` and `wgpu4k-android-jni` modules. Additionally, references to these modules were removed from the `settings.gradle.kts` file.

--- android-native-helper/src/androidMain/kotlin/io.ygdrasil.native.helper/Helper.kt
+++
@@ -1,9 +0,0 @@
-package io.ygdrasil.nativeHelper
-
-object Helper {
-    init {
-        System.loadLibrary("native_helper")
-    }
-
-    external fun nativeWindowFromSurface(androidSurface: android.view.Surface): Long
-}
