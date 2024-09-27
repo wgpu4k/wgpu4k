50d2e2a9c20acba246f794882d9d5dd29bd32888
ygdrasil-io <alexandre.mommers@gmail.com>
2024-09-27 14:30:37 +0200

    Refactor list mapping to use manual memory allocation
    Replaced `MemorySegment.ofArray` with manual allocation and setting of values using `ValueLayout.JAVA_INT`. This change enhances control over the memory layout and could potentially optimize performance for large lists.

--- wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/mapper/ListInt.kt
+++ wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/mapper/ListInt.kt
@@ -3,8 +3,10 @@ package io.ygdrasil.wgpu.mapper
 
 import java.lang.foreign.Arena
 import java.lang.foreign.MemorySegment
+import java.lang.foreign.ValueLayout
 
 internal fun Arena.map(input: List<Int>): MemorySegment {
     if (input.isEmpty()) return MemorySegment.NULL
-    return MemorySegment.ofArray(input.toIntArray())
+    return allocate((Int.SIZE_BYTES * input.size).toLong())
+        .also { output -> input.forEachIndexed { index, value -> output.setAtIndex(ValueLayout.JAVA_INT, index.toLong(), value) }}
 }
