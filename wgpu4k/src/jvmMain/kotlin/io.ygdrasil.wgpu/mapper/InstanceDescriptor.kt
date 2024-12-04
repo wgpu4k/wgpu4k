4fe853cf3cff2e564f3f5e8410968a02faa0f6a3
ygdrasil-io <alexandre.mommers@gmail.com>
2024-12-04 16:14:33 +0100

    Refactor InstanceDescriptor allocation logic
    Simplify the allocation and chaining logic within InstanceDescriptor by refactoring the handling of `nextInChain`. This change enhances readability and reduces the nested structure, ensuring `backends` and `chain.sType` are directly set, improving maintainability.

--- wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/mapper/InstanceDescriptor.kt
+++ wgpu4k/src/jvmMain/kotlin/io.ygdrasil.wgpu/mapper/InstanceDescriptor.kt
@@ -4,13 +4,11 @@ import ffi.MemoryAllocator
 import io.ygdrasil.wgpu.WGPUInstanceBackend
 import webgpu.WGPUInstanceDescriptor
 import webgpu.WGPUInstanceExtras
+import webgpu.WGPUNativeSType_InstanceExtras
 
 internal fun MemoryAllocator.map(backend: WGPUInstanceBackend) = WGPUInstanceDescriptor.allocate(this).also { output ->
-    WGPUInstanceExtras.allocate(this).also { nextInChain ->
-        WGPUInstanceExtras.backends(nextInChain, backend.value)
-        WGPUInstanceExtras.chain(nextInChain).also { chain ->
-            nextInChain.sType = WGPUSType_InstanceExtras()
-        }
-        output.nextInChain = nextInChain
-    }
+    output.nextInChain = WGPUInstanceExtras.allocate(this).also { nextInChain ->
+        nextInChain.backends = backend.value.toULong()
+        nextInChain.chain.sType = WGPUNativeSType_InstanceExtras
+    }.handler
 }
