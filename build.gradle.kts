fa59fbee133b570b0e04e6a018b9f6f00588f947
ygdrasil-io <alexandre.mommers@gmail.com>
2025-01-08 11:52:38 +0100

    Remove unused dependency resolution caching block
    Deleted a TODO-labeled block that was overriding default dependency caching. This cleanup improves the build script's clarity and maintains standardized configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,3 @@ allprojects {
 	group = "io.ygdrasil"
 	version = System.getenv("VERSION")?.takeIf { it.isNotBlank() } ?: "0.0.0-SNAPSHOT"
 }
-
-
-
-// TODO: remove later
-configurations.all {
-	resolutionStrategy {
-		cacheChangingModulesFor(0, "seconds")
-		cacheDynamicVersionsFor(0, "seconds")
-	}
-}
