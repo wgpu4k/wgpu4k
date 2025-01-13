10e426396996d3fcf73a2b4f9fc683597da0a47e
Alexandre Mommers <919982+ygdrasil-io@users.noreply.github.com>
2025-01-13 02:42:26 +0100

    fail test when e2e fail (#82)
    * Create necessary directories for e2e test tasks
    
    Ensure required directories ('js-chromium' and 'jvm') are created before executing e2e test tasks. This prevents potential errors during file operations and ensures smoother test execution.
    
    * Separate e2e test commands for Ubuntu with xvfb.
    
    Updated the workflow to include a conditional step for running e2e tests on 'ubuntu-latest' using xvfb-run. This ensures GUI-based tests are properly executed in Ubuntu environments while leaving other OS configurations unaffected.
    
    * Add WASM support for end-to-end tests with new resources
    
    Introduced support for WebAssembly (WASM) in the end-to-end (E2E) testing pipeline. Added new resources, updated project structure, and modified server and browser configurations to handle WASM execution. Enhanced `.gitignore` to account for WASM test result directories.

--- wgpu4k-scenes/src/wasmJsMain/kotlin/io.ygdrasil.wgpu.examples/AssetManager.wasmJs.kt
+++ wgpu4k-scenes/src/wasmJsMain/kotlin/io.ygdrasil.wgpu.examples/AssetManager.wasmJs.kt
@@ -4,9 +4,10 @@ import io.ygdrasil.webgpu.ImageBitmapHolder
 import korlibs.image.bitmap.Bitmap32
 import korlibs.image.color.ColorFormat
 import korlibs.io.file.Vfs
+import korlibs.io.file.std.resourcesVfs
 
 actual var customVfs: Vfs
-    get() = TODO("Not yet implemented")
+    get() = resourcesVfs.vfs // only to avoid fail of e2e test on browser
     set(value) {}
 
 internal actual fun Bitmap32.toBitmapHolder(textureFormat: ColorFormat): ImageBitmapHolder {
