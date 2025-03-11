1d264b50bea024b2310c7497b0e962111ef57f1b
ygdrasil-io <alexandre.mommers@gmail.com>
2025-03-11 01:42:16 +0100

    Remove unused RenderPassTimestampWrites class
    The RenderPassTimestampWrites class was no longer in use and has been removed to clean up the codebase. This helps reduce unnecessary complexity and improves maintainability.

--- wgpu4k/src/commonMain/kotlin/RenderPassDescriptor.kt
+++
@@ -1,9 +0,0 @@
-package io.ygdrasil.webgpu
-
-expect class RenderPassTimestampWrites : GPURenderPassTimestampWrites {
-    override val beginningOfPassWriteIndex: GPUSize32?
-    override val endOfPassWriteIndex: GPUSize32?
-    override val querySet: GPUQuerySet
-}
-
-
