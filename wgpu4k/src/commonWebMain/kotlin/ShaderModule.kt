8cd28f45910915c4529cac038e381619eb77ce9a
ygdrasil-io <alexandre.mommers@gmail.com>
2025-04-15 00:14:53 +0200

    Implement shader module compilation info mapping
    Added support for mapping `WGPUCompilationInfo` to `GPUCompilationInfo`, enabling proper handling of shader compilation messages. Refactored `getCompilationInfo` to return mapped results for better functionality and improved maintainability.

--- wgpu4k/src/commonWebMain/kotlin/ShaderModule.kt
+++ wgpu4k/src/commonWebMain/kotlin/ShaderModule.kt
@@ -1,12 +1,19 @@
 package io.ygdrasil.webgpu
 
+import io.ygdrasil.webgpu.mapper.map
+
 actual class ShaderModule(val handler: WGPUShaderModule) : GPUShaderModule {
     actual override var label: String
         get() = handler.label
-        set(value) { handler.label = value }
+        set(value) {
+            handler.label = value
+        }
 
-    actual override suspend fun getCompilationInfo(): Result<GPUCompilationInfo> {
-        TODO("Not yet implemented")
+    actual override suspend fun getCompilationInfo(): Result<GPUCompilationInfo> = runCatching {
+        handler
+            .getCompilationInfo()
+            .wait<WGPUCompilationInfo>()
+            .let { map(it) }
     }
 
     actual override fun close() {
