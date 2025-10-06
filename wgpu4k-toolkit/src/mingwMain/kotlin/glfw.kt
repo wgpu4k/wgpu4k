a07fcb3ccfa6972ce5f3fc40634fa7a3a02e156f
ygdrasil-io <alexandre.mommers@gmail.com>
2025-10-06 23:09:27 +0200

    Refactor `getSurface` to `getNativeSurface` in `glfw.kt` for improved readability and usage of `NativeAddress`.

--- wgpu4k-toolkit/src/mingwMain/kotlin/glfw.kt
+++ wgpu4k-toolkit/src/mingwMain/kotlin/glfw.kt
@@ -3,15 +3,24 @@
 package io.ygdrasil.webgpu
 
 import cnames.structs.GLFWwindow
+import ffi.NativeAddress
 import glfw.glfwGetWin32Window
-import kotlinx.cinterop.*
+import kotlinx.cinterop.COpaque
+import kotlinx.cinterop.COpaquePointer
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.interpretCPointer
+import kotlinx.cinterop.invoke
+import kotlinx.cinterop.reinterpret
 import platform.windows.GetModuleHandle
 
-actual fun WGPU.getSurface(window: CPointer<GLFWwindow>, sizeProvider: () -> Pair<Int, Int>): Surface {
+actual fun WGPU.getNativeSurface(window: CPointer<GLFWwindow>): NativeSurface {
     val hwnd = glfwGetWin32Window(window)  ?: error("fail to get hwnd")
     val hinstance: COpaquePointer = GetModuleHandle?.invoke(null)
         ?.let { interpretCPointer<COpaque>(it.rawValue) }
         ?.reinterpret() ?: error("fail to get hinstance")
-    val surface = getSurfaceFromWindows(hinstance, hwnd) ?: error("fail to get surface on Windows")
-    return Surface(surface, sizeProvider)
+    return getSurfaceFromWindows(
+        hinstance.let(::NativeAddress),
+        hwnd.let(::NativeAddress)
+    ) ?: error("fail to get surface on Windows")
 }
