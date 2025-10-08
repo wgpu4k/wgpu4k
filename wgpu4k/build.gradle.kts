7cf00a932b58af082ae016c57fef9610d1090563
ygdrasil-io <alexandre.mommers@gmail.com>
2025-10-08 02:43:50 +0200

    Re-enable commented native target configurations in `wgpu4k`'s `build.gradle.kts`.

--- wgpu4k/build.gradle.kts
+++ wgpu4k/build.gradle.kts
@@ -41,16 +41,14 @@ kotlin {
         //publishLibraryVariants("release", "debug")
     }
 
-    /*iosX64()
+    iosX64()
     iosArm64()
     iosSimulatorArm64()
     macosArm64()
     macosX64()
     linuxArm64()
-
-    mingwX64()*/
     linuxX64()
-
+    mingwX64()
 
     @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
     wasmJs {
