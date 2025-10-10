06550941910980d3e93fea14566bd6d8dffb43aa
ygdrasil-io <alexandre.mommers@gmail.com>
2025-10-10 06:45:05 +0200

    Enhance `asVsfFile` to fall back to `standardVfs` when file is missing in `resourcesVfs`.

--- wgpu4k-scenes/src/commonNativeMain/kotlin/AssetManager.commonNative.kt
+++ wgpu4k-scenes/src/commonNativeMain/kotlin/AssetManager.commonNative.kt
@@ -2,6 +2,7 @@ package io.ygdrasil.webgpu.examples
 
 import korlibs.io.file.VfsFile
 import korlibs.io.file.std.resourcesVfs
+import korlibs.io.file.std.standardVfs
 
 actual suspend fun String.asVsfFile(): VfsFile = resourcesVfs[this].takeIf { it.exists() }
     ?: standardVfs[this]
