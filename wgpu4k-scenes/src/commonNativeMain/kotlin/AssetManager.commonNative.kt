2947d44fd8bbbb05d0c56785fc0b2f93abdfebf7
ygdrasil-io <alexandre.mommers@gmail.com>
2025-10-13 23:14:35 +0200

    Consolidate `toBitmapHolder` implementation under `commonNativeMain` and remove platform-specific variants

--- wgpu4k-scenes/src/commonNativeMain/kotlin/AssetManager.commonNative.kt
+++ wgpu4k-scenes/src/commonNativeMain/kotlin/AssetManager.commonNative.kt
@@ -1,8 +1,25 @@
 package io.ygdrasil.webgpu.examples
 
+import ffi.globalMemory
+import io.ygdrasil.webgpu.ImageBitmapHolder
+import korlibs.image.bitmap.Bitmap32
+import korlibs.image.color.ColorFormat
 import korlibs.io.file.VfsFile
 import korlibs.io.file.std.resourcesVfs
 import korlibs.io.file.std.rootLocalVfs
 
 actual suspend fun String.asVsfFile(): VfsFile = resourcesVfs[this].takeIf { it.exists() }
     ?: rootLocalVfs[this].takeIf { it.exists() } ?: error("fail to get file from file system")
+
+internal actual fun Bitmap32.toBitmapHolder(textureFormat: ColorFormat): ImageBitmapHolder {
+    val bytes = extractBytes(textureFormat)
+    val buffer = globalMemory.allocateBuffer((Byte.SIZE_BYTES * bytes.size).toULong()).apply {
+        writeBytes(bytes)
+    }
+    return ImageBitmapHolder(
+        globalMemory,
+        buffer.handler,
+        width.toUInt(),
+        height.toUInt()
+    )
+}
