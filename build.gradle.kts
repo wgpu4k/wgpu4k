d90a193b9820e46a762b85056dcc95d3514bc397
Alexandre Mommers <alexandre.mommers@gmail.com>
2025-10-28 19:55:54 +0100

    Configure Yarn plugin to warn on lockfile mismatches and disable auto-replacement

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
@@ -8,6 +10,13 @@ plugins {
 allprojects {
 	group = "io.ygdrasil"
 	version = System.getenv("VERSION")?.takeIf { it.isNotBlank() } ?: "0.1.1-SNAPSHOT"
+}
+
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+    rootProject.the<YarnRootExtension>().yarnLockMismatchReport =
+        YarnLockMismatchReport.WARNING
+    rootProject.the<YarnRootExtension>().reportNewYarnLock = false
+    rootProject.the<YarnRootExtension>().yarnLockAutoReplace = false
 }
 
 configurations.all {
