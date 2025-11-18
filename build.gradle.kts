b1e9bfe1551f2211ab52a2bc9a7b3207c057441c
Alexandre Mommers <919982+ygdrasil-io@users.noreply.github.com>
2025-11-18 18:35:12 +0100

    Update project version to 0.2.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 allprojects {
 	group = "io.ygdrasil"
-	version = System.getenv("VERSION")?.takeIf { it.isNotBlank() } ?: "0.1.1-SNAPSHOT"
+	version = System.getenv("VERSION")?.takeIf { it.isNotBlank() } ?: "0.2.0-SNAPSHOT"
 }
 
 configurations.all {
